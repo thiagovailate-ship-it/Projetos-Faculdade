c20792ef5bb6e2240c6bd759a80ff04e7e440ad9
Thiago Vailate <thiago.vailate@aluno.fmpsc.edu.br>
2026-08-10 15:50:40 -0300

    Cadastro simples em python
    Um sistema de cadastro simples em Python, ele pede:
     
    - Nome do usuário
    - E-mail
    - Senha
    
    Ele imprime: "Cadrasto realizado!" e depois: "Bem-vindo(a) + Nome"

---
+++ testes.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+email = input("Digite seu e-mail: ")
+senha = input("Digite uma senha: ")
+
+print("Cadastro realizado com sucesso!")
+print("Bem-vindo(a),", nome)
