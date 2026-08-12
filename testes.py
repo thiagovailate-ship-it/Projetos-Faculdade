87c60547acd4b658d937a7a089b4092279405505
Thiago Vailate <thiago.vailate@aluno.fmpsc.edu.br>
2026-08-12 08:12:40 -0300

    Use getpass for password input

--- testes.py
+++ testes.py
@@ -1,6 +1,9 @@
+import getpass
+
 nome = input("Digite seu nome: ")
 email = input("Digite seu e-mail: ")
-senha = input("Digite uma senha: ")
+senha = getpass.getpass("Digite uma senha: ")
+confirmacao = getpass.getpass("Confirme sua senha: ")
 
 print("Cadastro realizado com sucesso!")
 print("Bem-vindo(a),", nome)
